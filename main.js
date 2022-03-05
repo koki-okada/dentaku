2445fa0f6d61aba0b36ad871faf8ccf4d275d121
koki-okada <68902213+koki-okada@users.noreply.github.com>
2022-03-05 10:08:24 +0900

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@
     }
     
     
-// 入力チェックの関数(一番先頭の値が0か演算子だったら、初期化)
+// 入力チェックの関数(先頭の値が0か演算子だったら、初期化)
 function input_check(){
   if(document.calc.display.value.slice( 0, 1 ) == "0"){
       document.calc.display.value = "";
@@ -26,19 +26,15 @@ function input_check(){
     }
 }
 
-// 入力チェックの関数(一番末尾が演算子だったら、演算子を上書き)
+// 入力チェックの関数(末尾２桁が演算子だったら、演算子を上書き)
 function input_check2(){
-  if(document.calc.display.value.slice( -1 ) == "+"){
-      document.calc.display.value.slice( -1 ).val("");
-      document.calc.display.value.slice( -1 ).val("+");
-    }else if(document.calc.display.value.slice( -1 ) == "-"){
-      document.calc.display.value.slice( -1 ).val("");
-      document.calc.display.value.slice( -1 ).val("-");
-    }else if(document.calc.display.value.slice( -1 ) == "*"){
-      document.calc.display.value.slice( -1 ).val("");
-      document.calc.display.value.slice( -1 ).val("*");
-    }else if(document.calc.display.value.slice( -1 ) == "/"){
-      document.calc.display.value.slice( -1 ).val("");
-      document.calc.display.value.slice( -1 ).val("/");
+  let operator = new Array('+', '-', '*', '/');
+  let operator_check = document.calc.display.value.slice( -2 );
+  let display_operator = document.calc.display.value.slice( -1 );
+  
+  // 末尾２桁(operator_check)に演算子(operator)が含まれる場合、末尾２桁を削除して、末尾の演算子(display_operator)を入力する
+  if(document.calc.display.value.slice( -2 ) == operator.inclueds(operator_check)){
+      document.calc.display.value.slice( -2 ).val("");
+      document.calc.display.value.slice( -1 ).val(display_operator);
     }
 }
