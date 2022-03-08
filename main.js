164d7c402244a573dbfc1576ea7c8567e65beede
koki-okada <68902213+koki-okada@users.noreply.github.com>
2022-03-08 09:22:57 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,35 +6,29 @@
       } else {
         document.calc.display.value += btn.value;
         input_check();
-        input_check2();
+        
       }
     }
     
-    
-// 入力チェックの関数(先頭の値が0か演算子だったら、初期化)
+// 入力チェックの関数
+//先頭の値が0か演算子の場合は初期化（「−」のみ先頭を許可)
+
 function input_check(){
   if(document.calc.display.value.slice( 0, 1 ) == "0"){
       document.calc.display.value = "";
     }else if(document.calc.display.value.slice( 0, 1 ) == "+"){
-      document.calc.display.value = "";
-    }else if(document.calc.display.value.slice( 0, 1 ) == "-"){
       document.calc.display.value = "";
     }else if(document.calc.display.value.slice( 0, 1 ) == "*"){
       document.calc.display.value = "";
     }else if(document.calc.display.value.slice( 0, 1 ) == "/"){
       document.calc.display.value = "";
     }
-}
-
-// 入力チェックの関数(末尾２桁が演算子だったら、演算子を上書き)
-function input_check2(){
-  let operator = new Array('+', '-', '*', '/');
-  let operator_check = document.calc.display.value.slice( -2 );
-  let display_operator = document.calc.display.value.slice( -1 );
-  
-  // 末尾２桁(operator_check)に演算子(operator)が含まれる場合、末尾２桁を削除して、末尾の演算子(display_operator)を入力する
-  if(document.calc.display.value.slice( -2 ) == operator.inclueds(operator_check)){
-      document.calc.display.value.slice( -2 ).val("");
-      document.calc.display.value.slice( -1 ).val(display_operator);
+    
+    // (末尾２桁が演算子だったら、演算子を上書き)
+    let operator = ['++', '+-', '+*', '+/', '--', '-+', '-*', '-/', '**', '*+', '*-', '*/', '//', '/+', '/-', '/*'];
+    
+      // 末尾２桁が演算子の場合、末尾の演算子だけを削除
+      if(operator.includes(document.calc.display.value.slice( -2 ))){
+            document.calc.display.value = document.getElementById("display").value.slice(0, -1);
+        }
     }
-}
